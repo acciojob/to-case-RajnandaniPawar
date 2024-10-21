1f5325a3ada95b10cbd8cd946de67f28301e841d
RajnandaniPawar <164873503+RajnandaniPawar@users.noreply.github.com>
2024-10-21 23:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function toCase(text) {
   // write your code here
+	    if (text === '') {
+        return '-'; // Return '-' for empty strings
+    }
+    
+    // Convert the string to lowercase and uppercase
+    const lowerCaseStr = text.toLowerCase();
+    const upperCaseStr = text.toUpperCase();
+    
+    // Return the concatenated result with '-' as a delimiter
+    return lowerCaseStr + '-' + upperCaseStr;
 }
 
 // DO not change the code below
